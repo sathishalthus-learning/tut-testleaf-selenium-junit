7b5be6d14c202976ddf8b0f54d5d3eaf34fb2a5a
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-02-20 11:16:17 +0530

    5,6

---
+++ src/test/java/alt/playground/Test05_Button.java
@@ -0,0 +1,133 @@
+package alt.playground;
+
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v118.dom.model.Rect;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test05_Button {
+	//
+	WebDriver driver;
+
+	//
+	@BeforeTest
+	public void setUp() {
+		//
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+	}
+
+	//
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	//
+	@Test
+	public void clickAndConfirmTitle() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_button")).click();
+		//
+		driver.findElement(By.id("j_idt88:j_idt90")).click();
+		assertTrue(driver.getTitle().contentEquals("Dashboard"));
+	}
+	
+	//
+	@Test
+	public void confirmButtonDisabled() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_button")).click();
+		//
+//		driver.findElement(By.id("j_idt88:j_idt92")).click();
+		assertFalse(driver.findElement(By.id("j_idt88:j_idt92")).isEnabled());
+	}
+	
+	//
+	@Test
+	public void findThePositionOfButton() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_button")).click();
+		//
+		WebElement submitButton = driver.findElement(By.id("j_idt88:j_idt94"));
+		Rectangle buttonPositions = submitButton.getRect();
+		System.out.println(buttonPositions.getDimension());
+		System.out.println(buttonPositions.getX());
+		System.out.println(buttonPositions.getY());
+	}
+	//
+	@Test
+	public void findTheButtonColor() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_button")).click();
+		//
+		WebElement saveButton = driver.findElement(By.id("j_idt88:j_idt96"));
+		System.out.println(saveButton.getCssValue("color"));
+	}
+	
+	//
+	@Test
+	public void findTheHeightAndWidth() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_button")).click();
+		//
+		WebElement saveButton = driver.findElement(By.id("j_idt88:j_idt98"));
+		Rectangle rect = saveButton.getRect();
+		System.out.println(rect.getHeight());
+		System.out.println(rect.getWidth());
+	}
+	
+	//
+	@Test
+	public void moveMouseOverAndConfirmColorChanges() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_button")).click();
+		//
+		WebElement testButton = driver.findElement(By.id("j_idt88:j_idt100"));
+		System.out.println(testButton.getCssValue("background-color"));
+		new Actions(driver).moveToElement(testButton).perform();
+		System.out.println(testButton.getCssValue("background-color"));
+	}
+	
+	//
+	@Test
+	public void clickImageAndHideClickAnywhere() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_button")).click();
+		//
+		WebElement testButton = driver.findElement(By.id("j_idt88:j_idt102:imageBtn"));
+		testButton.click();
+	}
+	
+	//
+	@Test
+	public void countRoundedButtons() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_button")).click();
+		//
+		int testButton = driver.findElements(By.className("rounded-button")).size();
+		System.out.println(testButton);
+	}
+}
---
+++ src/test/java/alt/playground/Test07_Checkbox.java
@@ -0,0 +1,121 @@
+package alt.playground;
+
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.time.Duration;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v118.dom.model.Rect;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test07_Checkbox {
+	//
+	WebDriver driver;
+
+	//
+	@BeforeTest
+	public void setUp() {
+		//
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+	}
+
+	//
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	//
+	@Test
+	public void selectFavoriteAutomationTool() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_dropdown")).click();
+		//
+		WebElement dropAutomation = driver.findElement(By.xpath("//select[@class='ui-selectonemenu']"));
+		Select automationList = new Select(dropAutomation);
+		automationList.selectByVisibleText("Playwright");
+	}
+
+	//
+	@Test
+	public void selectFavoriteCountry() {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_dropdown")).click();
+		//
+		WebElement dropAutomation = driver.findElement(By.id("j_idt87:country_input"));
+		Select countryList = new Select(dropAutomation);
+		countryList.selectByValue("Brazil"); // value in DOM
+	}
+	
+	//
+	@Test
+	public void selectFavoriteCountryCity() throws InterruptedException {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_dropdown")).click();
+		//
+		
+		Thread.sleep(3000);
+		WebElement dropAutomation1 = driver.findElement(By.id("j_idt87:country_input"));
+		Select countryList = new Select(dropAutomation1);
+		countryList.selectByVisibleText("Brazil");
+		Thread.sleep(3000);
+		WebElement dropAutomation2 = driver.findElement(By.id("j_idt87:city_input"));
+		Select cityList = new Select(dropAutomation2);
+		cityList.selectByVisibleText("Salvador");
+	}
+	
+	//
+	@Test
+	public void typeAndChooseCourse() throws InterruptedException {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_dropdown")).click();
+		//
+		driver.findElement(By.id("j_idt87:auto-complete_input")).sendKeys("AWS"+Keys.ENTER);
+		Thread.sleep(3000);
+	}
+	
+	//
+	@Test
+	public void chooseLanguageRandomly() throws InterruptedException {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_dropdown")).click();
+		//
+		WebElement languageDropdown = driver.findElement(By.id("j_idt87:lang_input"));
+		Select language = new Select(languageDropdown);
+		language.selectByIndex(3);
+		Thread.sleep(3000);
+	}
+	
+	//
+	@Test
+	public void chooseLanguageAndSelectItem2() throws InterruptedException {
+		//
+		driver.findElement(By.id("menuform:j_idt40")).click();
+		driver.findElement(By.id("menuform:m_dropdown")).click();
+		//
+		WebElement languageDropdown = driver.findElement(By.id("j_idt87:lang_input"));
+		Select language = new Select(languageDropdown);
+		language.selectByIndex(3);
+		Thread.sleep(3000);
+	}
+}
